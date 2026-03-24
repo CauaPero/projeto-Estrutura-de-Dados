62f93c7a900ecb5a1a3da4576e8285809cbec9a7
CauaPero <cauaprodrigues4@gmail.com>
2026-03-24 17:01:49 +0000

    enqueue implementado

--- src/FilaChamados.java
+++ src/FilaChamados.java
@@ -1,3 +1,20 @@
 public class FilaChamados {
+    private Chamado[] fila;
+    private int inicio;
+    private int fim;
 
+    public FilaChamados() {
+        fila = new Chamado[40];
+        inicio = 0;
+        fim = -1;     
+    }
+
+    public void enqueue(Chamado chamado) {
+        if(fim == fila.length - 1) {
+            System.out.println("Fila cheia");
+        } else {
+            fim++;
+            fila[fim] = chamado;
+        }
+    }
 }
